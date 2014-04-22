eecd871d678f333c039a03baaf88dbbf3fa79014
mrvincent <pk0119@gmail.com>
2014-04-21 21:28:30 -0400

    change color

--- Classes/BgLayer.cpp
+++ Classes/BgLayer.cpp
@@ -5,25 +5,27 @@ USING_NS_CC;
 USING_NS_CC_EXT;
 using namespace cocostudio;
 
-BgLayer::BgLayer()
-{
-    CCLOG("i am here 3");}
+BgLayer::BgLayer() {
+    CCLOG("i am here 3");
+}
 
-BgLayer::~BgLayer()
-{}
+BgLayer::~BgLayer() {
+}
 
 bool BgLayer::init() {
-    bool ret=false;
+    bool ret = false;
     CCLOG("i am here 2");
     do {
         CC_BREAK_IF(!Layer::init());
 
         //bgLayer
-        cocos2d::Node* node = SceneReader::getInstance()->createNodeWithSceneFile("RPGGame.json");
+        cocos2d::Node* node =
+                SceneReader::getInstance()->createNodeWithSceneFile(
+                        "RPGGame.json");
         this->addChild(node);
         CCLOG("i am here");
         ret = true;
-    } while(0);
+    } while (0);
 
     return ret;
 }
